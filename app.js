e596fa235f5c423857663c57603c1bc5ae1d2a18
Marc Rubia <marc@billymob.com>
2016-12-11 13:29:22 +0100

    convo

--- app.js
+++ app.js
@@ -18,9 +18,25 @@ var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
 // Create bot dialogs
-bot.dialog('/', function (session) {
-    session.send("Hello World");
+bot.add('/', function (session) {
+    if (!session.userData.name) {
+        session.beginDialog('/profile');
+    } else {
+        session.send('Hello %s!', session.userData.name);
+    }
 });
+bot.add('/profile', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
+
+
+
 server.get('/', restify.serveStatic({
  directory: __dirname,
  default: '/index.html'
