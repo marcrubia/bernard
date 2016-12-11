6e7a681fd85ffc25bce64e194f50c2dc9f38a90b
Marc Rubia <marc@billymob.com>
2016-12-11 13:32:31 +0100

    fix

--- app.js
+++ app.js
@@ -14,10 +14,14 @@ var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
-var bot = new builder.UniversalBot(connector);
+//var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
 // Create bot dialogs
+/*bot.dialog('/', function (session) {
+    session.send("Hello World");
+});*/
+var bot = new builder.TextBot();
 bot.add('/', function (session) {
     if (!session.userData.name) {
         session.beginDialog('/profile');
