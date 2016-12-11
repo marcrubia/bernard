8575048e37906b190eb09931012c6f23c40ddb27
Marc Rubia <marc@billymob.com>
2016-12-11 12:43:59 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+// Add your requirements
+var restify = require('restify'); 
+var builder = require('botbuilder'); 
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.PORT || 3000, function() 
+{
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+// Create bot dialogs
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});
